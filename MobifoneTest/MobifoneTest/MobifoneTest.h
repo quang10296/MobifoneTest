89235a41589c2affd103fb58f5588edfd67e6555
quang10296 <duyquangnguyen10296@gmail.com>
2021-08-11 10:39:21 +0700

    add framework

---
+++ MobifoneTest/MobifoneTest/MobifoneTest.h
@@ -0,0 +1,18 @@
+//
+//  MobifoneTest.h
+//  MobifoneTest
+//
+//  Created by Quang Nguyễn  on 8/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MobifoneTest.
+FOUNDATION_EXPORT double MobifoneTestVersionNumber;
+
+//! Project version string for MobifoneTest.
+FOUNDATION_EXPORT const unsigned char MobifoneTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MobifoneTest/PublicHeader.h>
+
+
---
+++ MobifoneTest/MobifoneTest/TestFunc.swift
@@ -0,0 +1,20 @@
+//
+//  Sum.swift
+//  MobifoneTest
+//
+//  Created by Quang Nguyễn  on 8/11/21.
+//
+
+import Foundation
+
+public class TestFunc {
+    public static func sumTestFunc( numberA:Double, numberB:Double) -> Double {
+        print("sum : \((numberA + numberB))")
+        return (numberA + numberB)
+    }
+    
+    public static func subTestFunc( numberA:Double, numberB:Double) -> Double {
+        print("sub : \((numberA - numberB))")
+        return (numberA - numberB)
+    }
+}
